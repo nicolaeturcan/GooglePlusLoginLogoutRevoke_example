7d43c5ae0da23505dc066d0a23a455d6fccca05a
nicolae <nicolaeturcan@iesebre.com>
2014-12-03 12:39:11 +0100

    GooglePlus Login && out && revoke access example

---
+++ src/com/iesebre/dam2/pa201415/nicolae/MainActivity.java
@@ -0,0 +1,401 @@
+package com.iesebre.dam2.pa201415.nicolae;
+
+import java.io.InputStream;
+
+import android.app.Activity;
+import android.app.AlertDialog;
+import android.app.Dialog;
+import android.app.PendingIntent;
+import android.content.DialogInterface;
+import android.content.Intent;
+import android.content.IntentSender.SendIntentException;
+import android.graphics.Bitmap;
+import android.graphics.BitmapFactory; 
+import android.os.AsyncTask;
+import android.os.Bundle;
+import android.util.Log;
+import android.view.Menu;
+import android.view.View;
+import android.view.View.OnClickListener;
+import android.widget.Button;
+import android.widget.ImageView;
+import android.widget.LinearLayout;
+import android.widget.TextView;
+import android.widget.Toast;
+
+import com.google.android.gms.common.ConnectionResult;
+import com.google.android.gms.common.GooglePlayServicesUtil;
+import com.google.android.gms.common.SignInButton;
+import com.google.android.gms.common.api.GoogleApiClient;
+import com.google.android.gms.common.api.GoogleApiClient.ConnectionCallbacks;
+import com.google.android.gms.common.api.GoogleApiClient.OnConnectionFailedListener;
+import com.google.android.gms.common.api.ResultCallback;
+import com.google.android.gms.common.api.Status;
+import com.google.android.gms.plus.Plus;
+import com.google.android.gms.plus.model.people.Person;
+import com.iesebre.dam2.pa201415.nicolae.R;
+
+public class MainActivity extends Activity implements OnClickListener,
+		ConnectionCallbacks, OnConnectionFailedListener {
+
+	private static final int RC_SIGN_IN = 0;
+	// Logcat tag
+	private static final String TAG = "GooglePlusSignOn";
+
+	// Profile pic image size in pixels
+	private static final int PROFILE_PIC_SIZE = 400;
+
+	// Google client to interact with Google API
+	private GoogleApiClient mGoogleApiClient;
+	
+	private static final int DIALOG_PLAY_SERVICES_ERROR = 91;
+
+	/**
+	 * A flag indicating that a PendingIntent is in progress and prevents us
+	 * from starting further intents.
+	 */
+	private boolean mIntentInProgress;
+
+	private boolean mSignInClicked;
+
+	private ConnectionResult mConnectionResult;
+
+	private SignInButton btnSignIn;
+	private Button btnSignOut, btnRevokeAccess;
+	private ImageView imgProfilePic;
+	private TextView txtName, txtEmail;
+	private LinearLayout llProfileLayout;
+
+	@Override
+	protected void onCreate(Bundle savedInstanceState) {
+		Log.e(TAG, "onCreate!");
+		super.onCreate(savedInstanceState);
+		setContentView(R.layout.activity_main);
+
+		btnSignIn = (SignInButton) findViewById(R.id.btn_sign_in);
+		btnSignOut = (Button) findViewById(R.id.btn_sign_out);
+		btnRevokeAccess = (Button) findViewById(R.id.btn_revoke_access);
+		imgProfilePic = (ImageView) findViewById(R.id.imgProfilePic);
+		txtName = (TextView) findViewById(R.id.txtName);
+		txtEmail = (TextView) findViewById(R.id.txtEmail);
+		llProfileLayout = (LinearLayout) findViewById(R.id.llProfile);
+
+		// Button click listeners
+		btnSignIn.setOnClickListener(this);
+		btnSignOut.setOnClickListener(this);
+		btnRevokeAccess.setOnClickListener(this);
+
+		mGoogleApiClient = new GoogleApiClient.Builder(this)
+				.addConnectionCallbacks(this)
+				.addOnConnectionFailedListener(this).addApi(Plus.API)
+				.addScope(Plus.SCOPE_PLUS_LOGIN).build();
+		
+		Log.e(TAG, "onCreate End!");
+	}
+
+	protected void onStart() {
+		Log.e(TAG, "onStart!");
+		super.onStart();
+		mGoogleApiClient.connect();
+		Log.e(TAG, "onStart end!");
+	}
+
+	protected void onStop() {
+		Log.e(TAG, "onStop!");
+		
+		if (mGoogleApiClient.isConnected()) {
+			mGoogleApiClient.disconnect();
+		}
+		super.onStop();
+	}
+	
+	
+	/**
+	 * Method to resolve any signin errors
+	 * */
+	private void resolveSignInError() {
+		
+		PendingIntent signInIntent  = mConnectionResult.getResolution();
+		if (signInIntent != null) {
+			if (mConnectionResult.hasResolution()) {
+				try {
+					mIntentInProgress = true;
+					mConnectionResult.startResolutionForResult(this, RC_SIGN_IN);
+				} catch (SendIntentException e) {
+					mIntentInProgress = false;
+					mGoogleApiClient.connect();
+				}
+			}
+		} else {
+			showDialog(DIALOG_PLAY_SERVICES_ERROR);
+		}
+	}
+	
+	@Override
+	  protected Dialog onCreateDialog(int id) {
+	    switch(id) {
+	      case DIALOG_PLAY_SERVICES_ERROR:
+	        if (GooglePlayServicesUtil.isUserRecoverableError(mConnectionResult.getErrorCode())) {
+	          return GooglePlayServicesUtil.getErrorDialog(
+	        		  mConnectionResult.getErrorCode(),
+	              this,
+	              RC_SIGN_IN,
+	              new DialogInterface.OnCancelListener() {
+	                @Override
+	                public void onCancel(DialogInterface dialog) {
+	                  Log.e(TAG, "Google Play services resolution cancelled");
+	                }
+	              });
+	        } else {
+	          return new AlertDialog.Builder(this)
+	              .setMessage(R.string.play_services_error)
+	              .setPositiveButton(R.string.close,
+	                  new DialogInterface.OnClickListener() {
+	                    @Override
+	                    public void onClick(DialogInterface dialog, int which) {
+	                      Log.e(TAG, "Google Play services error could not be "
+	                          + "resolved: " + mConnectionResult.getErrorCode());
+	                    }
+	                  }).create();
+	        }
+	      default:
+	        return super.onCreateDialog(id);
+	    }
+	  }
+
+	@Override
+	public void onConnectionFailed(ConnectionResult result) {
+		Log.i(TAG, "onConnectionFailed: ConnectionResult.getErrorCode() = " + result.getErrorCode());
+		Log.i(TAG, "See error types in http://developer.android.com/reference/com/google/android/gms/common/ConnectionResult.html");
+		if (!result.hasResolution()) {
+			GooglePlayServicesUtil.getErrorDialog(result.getErrorCode(), this,
+					0).show();
+			return;
+		}
+		
+		if (result.getErrorCode() == ConnectionResult.SIGN_IN_REQUIRED) {
+			Log.i(TAG, "Error type: SIGN_IN_REQUIRED");
+		}
+		
+		if (result.getErrorCode() == ConnectionResult.NETWORK_ERROR ) {
+			Log.i(TAG, "Error type: NETWORK_ERROR ");
+		}
+
+		if (result.getErrorCode() == ConnectionResult.API_UNAVAILABLE) {
+	          // An API requested for GoogleApiClient is not available. The device's current
+	          // configuration might not be supported with the requested API or a required component
+	          // may not be installed, such as the Android Wear application. You may need to use a
+	          // second GoogleApiClient to manage the application's optional APIs.
+	    } else if (!mIntentInProgress) {
+	    	Log.i(TAG, "not IntentInProgress!");
+			// Store the ConnectionResult for later usage
+			mConnectionResult = result;
+
+			if (mSignInClicked) {
+				Log.i(TAG, "signInAlreadyClicked! Resolving sign error...");
+				// The user has already clicked 'sign-in' so we attempt to
+				// resolve all
+				// errors until the user is signed in, or they cancel.
+				resolveSignInError();
+			}
+		}
+		
+		Log.i(TAG, "onConnectionFailed end!");
+		//TODO: Sign OUT?
+	}
+
+	@Override
+	protected void onActivityResult(int requestCode, int responseCode,
+			Intent intent) {
+		if (requestCode == RC_SIGN_IN) {
+			if (responseCode != RESULT_OK) {
+				mSignInClicked = false;
+			}
+
+			mIntentInProgress = false;
+
+			if (!mGoogleApiClient.isConnecting()) {
+				mGoogleApiClient.connect();
+			}
+		}
+	}
+
+	@Override
+	public void onConnected(Bundle arg0) {
+		mSignInClicked = false;
+		Toast.makeText(this, "User is connected!", Toast.LENGTH_LONG).show();
+
+		// Get user's information
+		getProfileInformation();
+
+		// Update the UI after signin
+		updateUI(true);
+
+	}
+
+	/**
+	 * Updating the UI, showing/hiding buttons and profile layout
+	 * */
+	private void updateUI(boolean isSignedIn) {
+		if (isSignedIn) {
+			btnSignIn.setVisibility(View.GONE);
+			btnSignOut.setVisibility(View.VISIBLE);
+			btnRevokeAccess.setVisibility(View.VISIBLE);
+			llProfileLayout.setVisibility(View.VISIBLE);
+		} else {
+			btnSignIn.setVisibility(View.VISIBLE);
+			btnSignOut.setVisibility(View.GONE);
+			btnRevokeAccess.setVisibility(View.GONE);
+			llProfileLayout.setVisibility(View.GONE);
+		}
+	}
+
+	/**
+	 * Fetching user's information name, email, profile pic
+	 * */
+	private void getProfileInformation() {
+		try {
+			if (Plus.PeopleApi.getCurrentPerson(mGoogleApiClient) != null) {
+				Person currentPerson = Plus.PeopleApi
+						.getCurrentPerson(mGoogleApiClient);
+				String personName = currentPerson.getDisplayName();
+				String personPhotoUrl = currentPerson.getImage().getUrl();
+				String personGooglePlusProfile = currentPerson.getUrl();
+				String email = Plus.AccountApi.getAccountName(mGoogleApiClient);
+
+				Log.i(TAG, "Name: " + personName + ", plusProfile: "
+						+ personGooglePlusProfile + ", email: " + email
+						+ ", Image: " + personPhotoUrl);
+
+				txtName.setText(personName);
+				txtEmail.setText(email);
+
+				// by default the profile url gives 50x50 px image only
+				// we can replace the value with whatever dimension we want by
+				// replacing sz=X
+				personPhotoUrl = personPhotoUrl.substring(0,
+						personPhotoUrl.length() - 2)
+						+ PROFILE_PIC_SIZE;
+
+				new LoadProfileImage(imgProfilePic).execute(personPhotoUrl);
+
+			} else {
+				Toast.makeText(getApplicationContext(),
+						"Person information is null", Toast.LENGTH_LONG).show();
+			}
+		} catch (Exception e) {
+			e.printStackTrace();
+		}
+	}
+
+	@Override
+	public void onConnectionSuspended(int arg0) {
+		mGoogleApiClient.connect();
+		updateUI(false);
+	}
+
+	@Override
+	public boolean onCreateOptionsMenu(Menu menu) {
+		// Inflate the menu; this adds items to the action bar if it is present.
+		getMenuInflater().inflate(R.menu.main, menu);
+		return true;
+	}
+
+	/**
+	 * Button on click listener
+	 * */
+	@Override
+	public void onClick(View v) {
+		if (!mGoogleApiClient.isConnecting()) {
+			switch (v.getId()) {
+			case R.id.btn_sign_in:
+				// Signin button clicked
+				signInWithGplus();
+				break;
+			case R.id.btn_sign_out:
+				// Signout button clicked
+				signOutFromGplus();
+				break;
+			case R.id.btn_revoke_access:
+				// Revoke access button clicked
+				revokeGplusAccess();
+				break;
+			}
+		}
+	}
+
+	/**
+	 * Sign-in into google
+	 * */
+	private void signInWithGplus() {
+		if (!mGoogleApiClient.isConnecting()) {
+   		    // We only process button clicks when GoogleApiClient is not transitioning
+ 	        // between connected and not connected.
+			mSignInClicked = true;
+			resolveSignInError();
+		}
+	}
+
+	/**
+	 * Sign-out from google
+	 * */
+	private void signOutFromGplus() {
+		if (mGoogleApiClient.isConnected()) {
+			Plus.AccountApi.clearDefaultAccount(mGoogleApiClient);
+			mGoogleApiClient.disconnect();
+			mGoogleApiClient.connect();
+			updateUI(false);
+		}
+	}
+
+	/**
+	 * Revoking access from google
+	 * */
+	private void revokeGplusAccess() {
+		if (mGoogleApiClient.isConnected()) {
+			Plus.AccountApi.clearDefaultAccount(mGoogleApiClient);
+			Plus.AccountApi.revokeAccessAndDisconnect(mGoogleApiClient)
+					.setResultCallback(new ResultCallback<Status>() {
+						@Override
+						public void onResult(Status arg0) {
+							Log.e(TAG, "User access revoked!");
+							mGoogleApiClient.disconnect();
+							mGoogleApiClient.connect();
+							updateUI(false);
+						}
+
+					});
+		}
+	}
+
+	/**
+	 * Background Async task to load user profile picture from url
+	 * */
+	private class LoadProfileImage extends AsyncTask<String, Void, Bitmap> {
+		ImageView bmImage;
+
+		public LoadProfileImage(ImageView bmImage) {
+			this.bmImage = bmImage;
+		}
+
+		protected Bitmap doInBackground(String... urls) {
+			String urldisplay = urls[0];
+			Bitmap mIcon11 = null;
+			try {
+				InputStream in = new java.net.URL(urldisplay).openStream();
+				mIcon11 = BitmapFactory.decodeStream(in);
+			} catch (Exception e) {
+				Log.e(TAG, e.getMessage());
+				e.printStackTrace();
+			}
+			return mIcon11;
+		}
+
+		protected void onPostExecute(Bitmap result) {
+			bmImage.setImageBitmap(result);
+		}
+	}
+	
+	
+
+}
